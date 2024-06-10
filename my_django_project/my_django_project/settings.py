3ffbf2696bdd2bad55d3f38f07663cee424f10f3
Quno <20070001008@stu.yasar.edu.tr>
2024-06-10 20:49:28 +0300

    Azure

--- my_django_project/my_django_project/settings.py
+++ my_django_project/my_django_project/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'your-secret-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['your-app-name.azurewebsites.net', 'your-custom-domain.com']
+ALLOWED_HOSTS = ['20.79.107.2', 'localhost', 'https://imdb-clone.azurewebsites.net/']
 
 # Application definition
 
