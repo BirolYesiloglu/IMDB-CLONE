02b8e18b7e52584f38234ec5265a5d93998e2948
Quno <20070001008@stu.yasar.edu.tr>
2024-06-10 20:46:57 +0300

    Azure

--- my_django_project/my_django_project/settings.py
+++ my_django_project/my_django_project/settings.py
@@ -8,12 +8,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'your_secret_key'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'your-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['your-app-name.azurewebsites.net', 'your-custom-domain.com']
 
 # Application definition
 
